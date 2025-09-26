cc9b41c51b123ded46d8edd3d27cad634d2cb70b
sbx_user1051 <sbx_user1051@169.254.58.13>
2025-09-26 11:49:22 +0000

    Fri Sep 26 11:49:22 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,10 +2,39 @@
 class CheckoutSolution:
 
     # skus = unicode string
-    def checkout(self, skus) -> int:
+    def checkout(self, skus: str) -> int:
         # Prices and special offers
         prices = {"A": 50, "B": 30, "C": 20, "D": 15}
         special_offers = {
             "A": (3, 130), # (quantity, offer price)
             "B": (2, 45),
         }
+
+        # Validate input
+        if not isinstance(skus, str):
+            return -1
+
+        validate = [sku for sku in skus if sku not in prices]
+
+        if len(validate) > 0:
+            return -1
+
+        # Count items in skus
+        counts = {}
+        for sku in skus:
+            counts[sku] = counts.get(sku, 0) + 1
+
+        total = 0
+        # Apply special offers
+        for item, qty in counts.items():
+            if item not in special_offers:
+                total += qty * prices[item]
+                continue
+
+            offer_qty, offer_price = special_offers[item]
+            # Check how many offers apply
+            num_offers, remainder = divmod(qty, offer_qty)
+            total += num_offers * offer_price + remainder * prices[item]
+
+        return total
+
