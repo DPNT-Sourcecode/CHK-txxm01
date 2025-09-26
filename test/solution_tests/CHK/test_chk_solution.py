57faf704551ade66a148f00540b1fbf3d8de7841
sbx_user1051 <sbx_user1051@169.254.58.13>
2025-09-26 13:31:37 +0000

    Fri Sep 26 13:31:37 UTC 2025

--- test/solution_tests/CHK/test_chk_solution.py
+++ test/solution_tests/CHK/test_chk_solution.py
@@ -7,18 +7,19 @@ class TestCheckoutSolution():
     def test_checkout_success(self):
         assert CheckoutSolution().checkout("A B") == 80
 
-    # def test_checkout_success_with_ee(self):
-    #     assert CheckoutSolution().checkout("A B EE") == 130
+    def test_checkout_success_with_ee(self):
+        assert CheckoutSolution().checkout("A B EE") == 130
 
-    # def test_checkout_success_with_fff(self):
-    #     assert CheckoutSolution().checkout("A B FFF") == 100
+    def test_checkout_success_with_fff(self):
+        assert CheckoutSolution().checkout("A B FFF") == 100
 
-    # def test_checkout_success_not_spaces(self):
-    #     assert CheckoutSolution().checkout("AC") == 70
+    def test_checkout_success_not_spaces(self):
+        assert CheckoutSolution().checkout("AC") == 70
 
-    # def test_checkout_fail_invalid_input(self):
-    #     assert CheckoutSolution().checkout("CFG") == -1
+    def test_checkout_fail_invalid_input(self):
+        assert CheckoutSolution().checkout("152") == -1
 
-    # def test_checkout_fail_input_int(self):
-    #     assert CheckoutSolution().checkout(100) == -1
+    def test_checkout_fail_input_int(self):
+        assert CheckoutSolution().checkout(100) == -1
+
 
