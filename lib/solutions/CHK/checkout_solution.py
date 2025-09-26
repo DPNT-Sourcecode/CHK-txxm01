b5d0f92d76fff5c079a329c20aa385c3733381a5
sbx_user1051 <sbx_user1051@169.254.58.13>
2025-09-26 13:40:37 +0000

    Fri Sep 26 13:40:37 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,10 +4,10 @@ class CheckoutSolution:
         # Unit prices
         self.prices = {
             "A": 50, "B": 30, "C": 20, "D": 15, "E": 40, "F": 10,
-            "G": 20, "H": 10, "I": 35, "J": 60, "K": 80, "L": 90,
+            "G": 20, "H": 10, "I": 35, "J": 60, "K": 70, "L": 90,
             "M": 15, "N": 40, "O": 10, "P": 50, "Q": 30, "R": 50,
-            "S": 30, "T": 20, "U": 40, "V": 50, "W": 20, "X": 90,
-            "Y": 10, "Z": 50,
+            "S": 20, "T": 20, "U": 40, "V": 50, "W": 20, "X": 17,
+            "Y": 20, "Z": 21,
         }
         # Multi-pack offers (largest-first will be enforced at runtime)
         # Each entry is list of tuples: (pack_qty, pack_price)
@@ -15,7 +15,7 @@ class CheckoutSolution:
             "A": [(5, 200), (3, 130)],
             "B": [(2, 45)],
             "H": [(10, 80), (5, 45)],
-            "K": [(2, 150)],
+            "K": [(2, 120)],        # updated
             "P": [(5, 200)],
             "Q": [(3, 80)],
             "V": [(3, 130), (2, 90)],
@@ -105,5 +105,6 @@ class CheckoutSolution:
         total += remaining * self.prices[sku]
 
         return total
+
 
 
