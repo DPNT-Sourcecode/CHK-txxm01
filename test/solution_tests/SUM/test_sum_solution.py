88ee7a8ce6968ae0a122e50002d5e34ffc5435cd
sbx_user1051 <sbx_user1051@169.254.58.13>
2025-09-26 10:42:25 +0000

    Fri Sep 26 10:42:25 UTC 2025

--- test/solution_tests/SUM/test_sum_solution.py
+++ test/solution_tests/SUM/test_sum_solution.py
@@ -4,12 +4,10 @@ from lib.solutions.SUM.sum_solution import SumSolution
 
 
 class TestSum():
-    def setUp(self):
-        self.sum_solution = SumSolution()
-
     def test_sum(self):
-        assert self.sum_solution.compute(1, 2) == 3
+        assert SumSolution().compute(1, 2) == 3
 
     def test_sum_error(self):
         with pytest.raises(TypeError):
             SumSolution().compute('a', 2)
+
