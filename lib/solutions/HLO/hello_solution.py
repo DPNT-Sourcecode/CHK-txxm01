42a58d867278e52f34149b259c6dbb02a19cb673
sbx_user1051 <sbx_user1051@169.254.58.13>
2025-09-26 11:10:18 +0000

    Fri Sep 26 11:10:18 UTC 2025

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -3,10 +3,6 @@ class HelloSolution:
 
     # friend_name = unicode string
     def hello(self, friend_name: str) -> str:
-        if not isinstance(friend_name, str):
-            raise TypeError("Expected argument to be a string")
-
-        # return f"Hello {friend_name}"
         return "Hello, World!"
 
 
--- test/solution_tests/HLO/test_hello_solution.py
+++ test/solution_tests/HLO/test_hello_solution.py
@@ -5,11 +5,6 @@ from lib.solutions.HLO.hello_solution import HelloSolution
 
 class TestHelloSolution():
     def test_hello_success(self):
-        # assert HelloSolution().hello("Linda") == "Hello Linda"
         assert HelloSolution().hello("Linda") == "Hello, World!"
-
-    def test_hello_error(self):
-        with pytest.raises(TypeError):
-            HelloSolution().hello(2)
 
 
