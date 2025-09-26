f63dcac90ea5dcb685743e00dc084aa4808a000f
sbx_user1051 <sbx_user1051@169.254.58.13>
2025-09-26 10:33:25 +0000

    Fri Sep 26 10:33:25 UTC 2025

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,5 +1,9 @@
 
 class SumSolution:
     def compute(self, x: int, y: int):
+        if not (isinstance(x, int) or isinstance(y, int)):
+            raise ValueError("Expected both arguments to be integers!")
+
         return x + y
+
 
