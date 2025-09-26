77fda2e10a5d5e6150d6b3d6c2c51feb57a1533f
sbx_user1051 <sbx_user1051@169.254.58.13>
2025-09-26 11:00:18 +0000

    Fri Sep 26 11:00:18 UTC 2025

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -3,5 +3,9 @@ class HelloSolution:
 
     # friend_name = unicode string
     def hello(self, friend_name: str) -> str:
+        if not isinstance(friend_name, str):
+            return TypeError("Expected argument to be a string")
+
         return f"Hello {friend_name}"
+
 
---
+++ test/solution_tests/HLO/test_hello_solution.py
@@ -0,0 +1,20 @@
+import pytest
+
+from lib.solutions.HLO.hello_solution import HelloSolution
+
+
+class TestHelloSolution():
+    def test_sum(self):
+        assert SumSolution().compute(1, 2) == 3
+
+    def test_sum_error(self):
+        with pytest.raises(TypeError):
+            SumSolution().compute('a', 2)
+
+    def test_sum_error_negative_number(self):
+        with pytest.raises(ValueError):
+            SumSolution().compute(-3, 2)
+
+    def test_sum_error_number_above_100(self):
+        with pytest.raises(ValueError):
+            SumSolution().compute(3, 105)
