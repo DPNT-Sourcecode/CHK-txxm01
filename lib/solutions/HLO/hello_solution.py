167e0a8e7a85a0ff37d868116e3600d467c0e688
sbx_user1051 <sbx_user1051@169.254.58.13>
2025-09-26 11:08:58 +0000

    Fri Sep 26 11:08:58 UTC 2025

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -3,7 +3,9 @@ class HelloSolution:
 
     # friend_name = unicode string
     def hello(self, friend_name: str) -> str:
-        if not isinstance(friend_name, str):
-            raise TypeError("Expected argument to be a string")
+        # if not isinstance(friend_name, str):
+        #     raise TypeError("Expected argument to be a string")
 
-        return f"Hello {friend_name}"
+        # return f"Hello {friend_name}"
+        return "Hello, World!"
+
