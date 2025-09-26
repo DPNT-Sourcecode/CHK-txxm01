551927e63ffe050cba677667674aa021146c449d
sbx_user1051 <sbx_user1051@169.254.58.13>
2025-09-26 13:20:24 +0000

    Fri Sep 26 13:20:24 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,11 +1,21 @@
 
 class CheckoutSolution:
     def __init__(self):
-        self.prices = {"A": 50, "B": 30, "C": 20, "D": 15, "E": 40, "F": 10}
+        self.prices = {
+            "A": 50, "B": 30, "C": 20, "D": 15, "E": 40, "F": 10,
+            "G": 20, "H": 10, "I": 35, "J": 60, "K": 80, "L": 90,
+            "M": 15, "N": 40, "O": 10, "P": 50, "Q": 30, "R": 50,
+            "S": 30, "T": 20, "U": 40, "V": 50, "W": 20, "X": 90,
+            "Y": 10, "Z": 50,
+        }
         self.multi_offers = {
-        "A": [(5, 200), (3, 130)], # (quantity, offer price)
-        "B": [(2, 45)],
-            # C, D, E have no multi-pack price (E has a cross-item freebie instead)
+            "A": [(5, 200), (3, 130)], # (quantity, offer price)
+            "B": [(2, 45)],
+            "H": [(10, 80), (5, 45)],
+            "K": [(2, 150)],
+            "P": [(5, 200)],
+            "Q": [(3, 80)],
+            "V": [(3, 130), (2, 90)],
         }
 
     # skus = unicode string
@@ -74,5 +84,6 @@ class CheckoutSolution:
         total += remaining * self.prices[sku]
 
         return total
+
 
 
